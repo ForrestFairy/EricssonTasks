4d0e93191fed80933036da9933bfad6375f5d39d
ForrestFairy <s.bogusz@lancaster.ac.uk>
2021-02-04 14:58:20 +0100

    Now the logic is fine but the output is wrong

--- zadanie1.erl
+++ zadanie1.erl
@@ -4,7 +4,7 @@
 change(File) -> 
     {ok, Inbinary} = file:read_file(File),
     {All, Bad, Good} = process_input(Inbinary),
-    file:write_file("output.txt", [[All] ++ "\n" ++ [Bad] ++ "\n" ++ [Good]]),
+    file:write_file("output.txt", "~d\n~p\n~p", [All, Bad, Good]),
 
     {All, Bad, Good}.
 
@@ -25,12 +25,12 @@ process([Head | Tail], All, Bad, Good) ->
 check_if_good(Check) ->
     Control = Check rem 10,
     ID = Check div 10000,
-    io:fwrite("~p \n~p ~p", [ID, Control, ID rem 10]),
+    % io:fwrite("~p \n~p ~p", [ID, Control, ID rem 10]),
     is_good(Control, ID rem 10).
 
 %Control and should be the same as the last digit of ID in binary
 is_good(Control, Control) ->
-    io:fwrite("Hello \n"),
+    % io:fwrite("Hello \n"),
     true;
 is_good(_, _) ->
     false.
