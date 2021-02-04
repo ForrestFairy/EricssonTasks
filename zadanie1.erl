ffeccc71e7c0e481062b2d5bd17432f1d73c5410
ForrestFairy <s.bogusz@lancaster.ac.uk>
2021-02-04 14:39:32 +0100

    Created dane.txt to check if my program works correctly. It's nearly done bu need to chamge some stuff

--- zadanie1.erl
+++ zadanie1.erl
@@ -2,8 +2,35 @@
 -export([change/1]).
 
 change(File) -> 
-    {ok, Binary} = file:read_file(File),
-    split_binary(Binary).
+    {ok, Inbinary} = file:read_file(File),
+    {All, Bad, Good} = process_input(Inbinary),
+    file:write_file("output.txt", [[All] ++ "\n" ++ [Bad] ++ "\n" ++ [Good]]),
 
-split_binary(<<ID:4, Msg:3, Check:1, Tail/binary>>) ->
-    {ID, Msg, Check, Tail}.
+    {All, Bad, Good}.
+
+process_input(Inbinary) ->
+    process(tuple_to_list(split_binary(Inbinary, 8)), 0, 0, []).
+
+process([], All, Bad, Good) -> {All, Bad, Good};
+process([Head | Tail], All, Bad, Good) ->
+    
+    case check_if_good(binary_to_integer(Head)) of
+        false ->
+            process(Tail, All + 1, Bad + 1, Good);
+        true ->
+            process(Tail, All + 1, Bad, Good ++ binary_to_list(Head))
+        end.
+    
+
+check_if_good(Check) ->
+    Control = Check rem 10,
+    ID = Check div 10000,
+    io:fwrite("~p \n~p ~p", [ID, Control, ID rem 10]),
+    is_good(Control, ID rem 10).
+
+%Control and should be the same as the last digit of ID in binary
+is_good(Control, Control) ->
+    io:fwrite("Hello \n"),
+    true;
+is_good(_, _) ->
+    false.
