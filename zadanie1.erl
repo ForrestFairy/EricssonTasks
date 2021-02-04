339810295ee749ae394b4af6f2bbe9589f9cb7b2
ForrestFairy <szymon.bogusz55@gmail.com>
2021-02-04 16:31:53 +0000

    Started to change the programe because previously it was working only for 2 objects

--- zadanie1.erl
+++ zadanie1.erl
@@ -2,35 +2,57 @@
 -export([change/1]).
 
 change(File) -> 
+    %read from file
     {ok, Inbinary} = file:read_file(File),
+    
     {All, Bad, Good} = process_input(Inbinary),
-    file:write_file("output.txt", "~d\n~p\n~p", [All, Bad, Good]),
-
+    {ok, Output} = file:open("output.txt", [write]),
+    io:format(Output, "~p\n~p\n~p", [All, Bad, Good]),
     {All, Bad, Good}.
 
 process_input(Inbinary) ->
-    process(tuple_to_list(split_binary(Inbinary, 8)), 0, 0, []).
+    List = split_file(Inbinary),
+    process(List, 0, 0, []).
+
+split_file(Inbinary) ->
+    split_into_tuples(split_binary(Inbinary, 8), []).
+
+%here A, B as binaries
+split_into_tuples({A, B}, List) when size(B) == 0 ->
+    [List | binary_to_list(A)];
+split_into_tuples({A, B}, List) ->
+    split_into_tuples(split_binary(B, 8), [List | binary_to_list(A)]).
 
 process([], All, Bad, Good) -> {All, Bad, Good};
+
 process([Head | Tail], All, Bad, Good) ->
     
-    case check_if_good(binary_to_integer(Head)) of
+    case check_if_good(Head) of
         false ->
+            %if an object is bad count it and add to bad ones
             process(Tail, All + 1, Bad + 1, Good);
         true ->
-            process(Tail, All + 1, Bad, Good ++ binary_to_list(Head))
+            %if an object is good count it and add to the list of good ones
+            process(Tail, All + 1, Bad, [Good | binary_to_list(Head)])
+        end;
+process(Head, All, Bad, Good) ->
+    case check_if_good(Head) of
+        false ->
+            %if an object is bad count it and add to bad ones
+            {All + 1, Bad + 1, Good};
+        true ->
+            %if an object is good count it and add to the list of good ones
+            {All + 1, Bad, [Good | Head]}
         end.
-    
 
 check_if_good(Check) ->
     Control = Check rem 10,
     ID = Check div 10000,
-    % io:fwrite("~p \n~p ~p", [ID, Control, ID rem 10]),
     is_good(Control, ID rem 10).
 
-%Control and should be the same as the last digit of ID in binary
+% Control and should be the same as the last digit of ID in binary
+% because the last digit tells if binary is odd or even
 is_good(Control, Control) ->
-    % io:fwrite("Hello \n"),
     true;
 is_good(_, _) ->
     false.
